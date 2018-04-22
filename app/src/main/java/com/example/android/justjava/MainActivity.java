95e31e42ef861d22bfe5b6e82a06fc96fd802509
mecozma <mecozma@gmail.com>
2018-04-22 02:59:38 +0100

    Added checkboxes in the layout and updated the summary message

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,12 +9,14 @@
 package com.example.android.justjava;
 
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-import java.text.NumberFormat;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -33,8 +35,22 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price);
+
+        //Gets the name of the customer
+        EditText customerName = (EditText) findViewById(R.id.name_field);
+        String getCustomerName = customerName.getText().toString();
+        Log.v("Main", "Customer name is: " + getCustomerName);
+
+        //State of Whipped cream topping checkbox
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.whippedCream);
+        boolean hasWhippedCream = whippedCream.isChecked();
+
+        //State of Chocolate topping checkbox
+        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate);
+        Boolean hasChocolate = chocolate.isChecked();
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, getCustomerName);
         displayQuantity(numberOfCoffees);
         displayMessage(priceMessage);
     }
@@ -42,25 +58,40 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Calculates the price of the order.
      *
-     *@return total price
+     * @param hasWhippedCream Adds 1$ if the user adds whipped cream topping
+     * @param hasChocolate Adds 2$ if the user adds chocolate topping
+     * @return total price
      */
-    private int calculatePrice() {
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
 
-        int price = numberOfCoffees * 5;
-        return price;
+        int basePrice =  5;
+
+        if (hasWhippedCream) {
+            basePrice +=1;
+        }
+        if (hasChocolate) {
+            basePrice += 2;
+        }
+
+        return numberOfCoffees * basePrice;
     }
 
     /**
-     *  Order summary method
+     * Order summary method
      *
-     *  @param price
+     * @param price
+     * @param hasWhippedCream
+     * @param hasChocolate
+     * @param getCustomerName
      */
 
-    private String createOrderSummary (int price) {
-        String priceMessage = "Name: Kaptain Kunal" +
-                                    "\nQuantity: " + numberOfCoffees +
-                                    "\nTotal: " + numberOfCoffees * 5 +
-                                    "\nThank you!";
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate, String getCustomerName) {
+        String priceMessage = "Name: " + getCustomerName +
+                "\nAdd whipped Cream? " + hasWhippedCream +
+                "\nAdd Chocolate? " + hasChocolate +
+                "\nQuantity: " + numberOfCoffees +
+                "\nTotal: " + calculatePrice(hasWhippedCream, hasChocolate) +
+                "\nThank you!";
         return priceMessage;
     }
 
@@ -70,6 +101,12 @@ public class MainActivity extends AppCompatActivity {
     public void increment(View view) {
 
         numberOfCoffees = numberOfCoffees + 1;
+
+        if (numberOfCoffees > 100) {
+            numberOfCoffees = 100;
+            Toast.makeText(this,"You can't order more than 100 coffees", Toast.LENGTH_LONG).show();
+        }
+
         displayQuantity(numberOfCoffees);
     }
 
@@ -79,6 +116,13 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view) {
 
         numberOfCoffees = numberOfCoffees - 1;
+
+        if (numberOfCoffees < 1) {
+            numberOfCoffees = 1;
+            Toast.makeText(this, "You can't order less than one coffee!",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
         displayQuantity(numberOfCoffees);
     }
 
